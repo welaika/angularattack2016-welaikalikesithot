87c70eb9a6f9ab35791f0d417f0f21e235875753
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2016-05-14 12:54:24 +0200

    moved to NG1.5 + ES6

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('app', []);
+
+app.constant('config', {
+    apiUrl: '../api/'
+});
+
+
+app.config((statesProvider) => {
+    statesProvider.setPrefix('You are feeling');
+});
