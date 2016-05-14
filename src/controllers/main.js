3e720d514b5356108a4b6804228169b546e96810
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2016-05-14 18:29:15 +0200

    get data

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,7 +1,18 @@
 class MainController {
   /*@ngInject*/
-  constructor() {
+  constructor($log, HeroesRepoService) {
+    this.heroList = {}
+    HeroesRepoService.getHeroes()
+      .then((response) => {
+        this.heroList = response.data
+      })
+      .catch((response) => {
+        $log.error(`Error fetching heroes list: ${response.status} status code`)
+      })
+
   }
 }
 
-angular.module('hots').controller('MainController', MainController);
+angular
+  .module('hots')
+  .controller('MainController', MainController);
--- src/services/heroes-repo.js
+++ src/services/heroes-repo.js
@@ -1,13 +1,17 @@
 class HeroesRepoService {
-    /*@ngInject*/
     constructor($http, API) {
         this.$http = $http;
         this.API = API;
     }
     getHeroes() {
+      return this.$http.get(this.API.endpoint)
     }
 }
 
 angular
   .module('hots')
-  .factory('HeroesRepoService', HeroesRepoService)
+  .factory('HeroesRepoService', (() => {
+    let fn = ($http, API) => new HeroesRepoService($http, API);
+    fn.$inject = HeroesRepoService.$inject;
+    return fn;
+})())
