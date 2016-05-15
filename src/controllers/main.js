09c8aca190c1bbdb63a5e0f171e969dd4b5c8051
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2016-05-15 17:14:01 +0200

    heroes can be filtered by text name (exact match from the beginning)

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -3,11 +3,13 @@ class MainController {
   constructor($log, HeroesRepoService) {
     this.currentTab = 'allies'
     this.heroList = {}
+    this.searchText = undefined
+    this.filteredHeroList = {}
     this.stats = { allies: {complexity: 0, damage: 0, survivability: 0, utility: 0}, enemies: {complexity: 0, damage: 0, survivability: 0, utility: 0} }
 
     HeroesRepoService.getHeroes()
       .then((response) => {
-        this.heroList = response.data.map((hero) => {
+        this.filteredHeroList = this.heroList = response.data.map((hero) => {
           return Object.assign(hero, {
             selected: {
               allies: false,
@@ -34,6 +36,15 @@ class MainController {
 
   setCurrentTab(tabName) {
     this.currentTab = tabName;
+  }
+
+  search(needle = this.searchText) {
+    if (needle.length == 0) {
+      this.filteredHeroList = this.heroList;
+    }
+
+    let regex = new RegExp('^' + this.searchText + '', 'i')
+    this.filteredHeroList = this.heroList.filter((hero) => hero.name.match(regex) )
   }
 }
 
