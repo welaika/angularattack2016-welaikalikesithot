3675211ffac167a3af15e2ab3c7bb8c1549e4d27
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2016-05-15 21:19:45 +0200

    wip

--- src/directives/hero-search.js
+++ src/directives/hero-search.js
@@ -4,21 +4,20 @@ class HeroSearchDirective {
     this.restrict = 'A'
     this.scope = {
       searchText: "=",
-      heroList: "@",
+      heroList: "=",
       filteredHeroList: "="
     }
   }
 
   link(scope, element, attrs, ctrl) {
     scope.$watch('searchText', () =>{
-      let heroList = angular.fromJson(scope.heroList)
-      if (!heroList.length > 0){ return }
+      if (!scope.heroList.length > 0){ return }
       if (scope.searchText == undefined || scope.searchText.length == 0) {
         scope.filteredHeroList = scope.heroList;
       }
 
       let regex = new RegExp('^' + scope.searchText + '', 'i')
-      scope.filteredHeroList = angular.fromJson(scope.heroList).filter((hero) => hero.name.match(regex) )
+      scope.filteredHeroList = scope.heroList.filter((hero) => hero.name.match(regex) )
       scope.$applyAsync()
     })
   }
